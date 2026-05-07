e3800b428f359456650412b5249d2e3e5b0e7d3a
Chris Catton <christopher.catton@dna.inc>
2026-05-07 12:36:34 +0200

    chore: raw screen version without px (enables more performant usage in code)

--- packages/test-app-react/src/components/Theme/tokens.ts
+++ packages/test-app-react/src/components/Theme/tokens.ts
@@ -2,9 +2,14 @@ export type ThemeTokens = {
 	[namespace: string]: { [name: string]: string };
 };
 
+export const screen = {
+	width: 1920,
+	height: 1080,
+};
+
 export const tokens = {
 	screen: {
-		width: '1920px',
-		height: '1080px',
+		width: `${screen.width}px`,
+		height: `${screen.height}px`,
 	},
 } as const satisfies ThemeTokens;
